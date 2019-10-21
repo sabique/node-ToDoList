8d12bda365ca1fa0eb4f3e5f16a01527d3672e5f
Sabique <sabiquekhan@gmail.com>
2019-10-21 13:25:33 +0530

    Setup th methods to get the to-do item input value

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require('express');
-
 let app = express();
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -17,9 +17,9 @@ app.get('/', (req, res) => {
         <h1 class="display-4 text-center py-1">To-Do App</h1>
         
         <div class="jumbotron p-3 shadow-sm">
-          <form>
+          <form action="/create-item" method="POST">
             <div class="d-flex align-items-center">
-              <input autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
@@ -55,5 +55,10 @@ app.get('/', (req, res) => {
     </html>
     `);
 });
+
+app.post('/create-item', (req, res) => {
+    console.log(req.body.item);
+    res.send(`Thank you for submitting the form!`);
+})
 
 app.listen(3000);
