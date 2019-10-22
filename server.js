fe2f05ed2b365d111df3e05046f57fbe06b3f789
Sabique <sabiquekhan@gmail.com>
2019-10-23 00:26:16 +0530

    Updated the code for the dynamic port listener

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ let sanitizeHTML = require('sanitize-html');
 
 let app = express();
 let db;
+let port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 let connectionString = 'mongodb+srv://todoAppUser:todoAppUserPWD@clusternodejs-bt3dh.mongodb.net/TodoApp?retryWrites=true&w=majority';
 mongodb.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
     db = client.db();
-    app.listen(3000);
+    app.listen(port);
 });
 
 app.use(express.json());
